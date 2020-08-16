2a7f9d8eab96ba748291a088a3ed744616bc202a
CatalinP04 <katalin.popusoi@gmail.com>
2020-08-16 12:17:55 +0300

    Move components to separate files

---
+++ frontend-ssr/components/feedback-review/CommentBlock.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'next/link';
+import { Comment } from './interfaces';
+import styles from './comment-block.module.scss';
+
+export default function CommentBlock(props: Comment) {
+  return (
+    <div className={styles.comment}>
+      <header>
+        <h4>
+          <Link href="/bob" prefetch={false}>
+            <a>
+              <strong>{props.user}</strong>
+            </a>
+          </Link>
+          <span> a lasat urmatorul comentariu</span>
+        </h4>
+        <button>&#128077;</button>
+        <button>&#128078;</button>
+        <button>&#128172;</button>
+      </header>
+      <p>
+        {props.comment}
+      </p>
+    </div>
+  )
+}
--- frontend-ssr/pages/feedback-review/index.tsx
+++ frontend-ssr/pages/feedback-review/index.tsx
@@ -1,75 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
 
 import Header from '~/components/header/Header.component';
 import styles from '~/styles/pages/feedback-review.module.scss';
+import ExerciseArea from '~/components/feedback-review/ExerciseArea';
 
 
 /**
  * TODO:
- * 1. Move components
+ * 1. Move components - DONE
  * 2. Add button actions
- * 3. Add link to commenter profile
+ * 3. Add link to commenter profile - DONE, link to bob for now
  * 4. Create API functionality
  */
-
-interface Comment {
-  id: number;
-  user: string;
-  comment: string;
-};
-
-const MOCK_COMMENTS: Comment[] = [
-  { id: 1, user: 'Pava', comment: 'Lorem ipsum dolor sit amet.' },
-  { id: 2, user: 'Catalin Popusoi', comment: 'Pretium fusce id velit ut tortor pretium.' },
-  { id: 3, user: 'George-Marian Morosac', comment: 'Nibh ipsum consequat nisl vel pretium lectus quam.' },
-  { id: 4, user: 'Cristian Gatu', comment: 'Ac tincidunt vitae semper quis lectus nulla.' },
-  { id: 5, user: 'Dragos Silion', comment: 'Viverra maecenas accumsan lacus vel facilisis.' },
-];
-
-function Comment(props: Comment) {
-  return (
-    <div className={styles.comment}>
-      <header>
-        <h4>
-          <Link href="/bob" prefetch={false}>
-            <a>
-              <strong>{props.user}</strong>
-            </a>
-          </Link>
-          <span> a lasat urmatorul comentariu</span>
-        </h4>
-        <button>&#128077;</button>
-        <button>&#128078;</button>
-        <button>&#128172;</button>
-      </header>
-      <p>
-        {props.comment}
-      </p>
-    </div>
-  )
-}
-
-function ExerciseArea() {
-  return (
-    <section className={styles.exercise}>
-      <div className={styles.editor}>
-        <p>Code goes here</p>
-      </div>
-      <div className={styles.feedback}>
-        <header>
-          <h3>Comentarii</h3>
-          <button className="btn btn--blue">Aproba tot</button>
-          <button className="btn btn--danger">Respinge tot</button>
-        </header>
-        <section className={styles.comments}>
-          {MOCK_COMMENTS.map(comment => <Comment key={comment.id} {...comment} />)}
-        </section>
-      </div>
-    </section>
-  )
-}
 
 export default function FeedbackReview() {
   return (
