bf217d4823d4b810e2016f048d689f687610c785
CatalinP04 <katalin.popusoi@gmail.com>
2020-09-27 11:56:31 +0300

    Add colors to the variables file, add font awesome

--- frontend-ssr/components/feedback-review/CommentBlock.tsx
+++ frontend-ssr/components/feedback-review/CommentBlock.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
+import { faThumbsUp, faThumbsDown, faComments } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Comment } from './comment.model';
 import styles from './CommentBlock.module.scss';
 
@@ -15,9 +17,15 @@ export default function CommentBlock(props: Comment) {
           </Link>
           <span> a lasat urmatorul comentariu</span>
         </h4>
-        <button type="button">&#128077;</button>
-        <button type="button">&#128078;</button>
-        <button type="button">&#128172;</button>
+        <button type="button">
+          <FontAwesomeIcon icon={faThumbsUp} />
+        </button>
+        <button type="button">
+          <FontAwesomeIcon icon={faThumbsDown} />
+        </button>
+        <button type="button">
+          <FontAwesomeIcon icon={faComments} />
+        </button>
       </header>
       <p>
         {props.comment}
